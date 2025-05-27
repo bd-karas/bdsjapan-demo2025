e52200a626ab0b37d7ee88925fa9fc8ea9853542
karas <karas@blackduck.com>
2025-05-27 16:48:27 +0900

    Add a copy paste error

--- maven-demo/src/main/java/com/blackduck/app/App.java
+++ maven-demo/src/main/java/com/blackduck/app/App.java
@@ -7,4 +7,21 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Black World!");
     }
+
+}
+
+class TestCopyPasteError {
+    Boolean foo(int k) { return true; }
+    Boolean bar(int k) { return true; }
+
+    void stuff() { }
+
+    static int key1 = 3, key2 = 5;
+
+    void bar() {
+        if (foo(key1) && bar(key1)) { stuff(); }
+        // A COPY_PASTE_ERROR defect occurs here.
+        if (foo(key2) && bar(key1)) { stuff(); }
+    }
+
 }
