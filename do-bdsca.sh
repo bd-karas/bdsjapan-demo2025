7fef176554908145316b089921cfd4ad39596b66
karas <karas@blackduck.com>
2025-05-27 14:54:41 +0900

    Edit do-bdsca.sh to enable maven scan

--- do-bdsca.sh
+++ do-bdsca.sh
@@ -6,4 +6,8 @@ bash <(curl -sL https://detect.blackduck.com/detect10.sh) \
  --blackduck.api.token=$API_TOKEN \
  --detect.project.name=karas_test \
  --detect.project.version.name=test-maven01 \
-  --detect.source.path="."
+ --detect.source.path="." \
+ --logging.level.detect=TRACE \
+ --detect.excluded.detector.types=GIT \
+ --detect.detector.search.depth=3
+#--detect.maven.included.scopes=compile,test \  
